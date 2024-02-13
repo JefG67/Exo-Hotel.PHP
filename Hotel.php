63d7f233de5076e2e16996681cbdbdaba0096803
JefG67 <jefguilpain@gmail.com>
2024-02-13 18:05:35 +0100

    pas d'affichage function reservation

--- Hotel.php
+++ Hotel.php
@@ -1,31 +1,33 @@
 <?php
 
 class Hotel{
-    private string $hotel;
+    private string $nomHotel;
     private string $adresse;
-    private int $codePostal;
+    private string $codePostal;
     private string $ville;
     private array $chambres;
+    private array $reservations;
 
 
-    public function __construct(string $hotel, string $adresse, int $codePostal, string $ville){
-        $this->hotel = $hotel;
+    public function __construct(string $nomHotel, string $adresse, string $codePostal, string $ville){
+        $this->nomHotel = $nomHotel;
         $this->adresse = $adresse;
         $this->codePostal = $codePostal;
         $this-> ville = $ville;
         $this->chambres = [];
+        $this->reservations =[];
     }
 
         
-    public function getHotel()
+    public function getNomHotel()
     {
-        return $this->hotel;
+        return $this->nomHotel;
     }
 
   
-    public function setHotel($hotel)
+    public function setnNomHotel($nomHotel)
     {
-        $this->hotel = $hotel;
+        $this->nomHotel = $nomHotel;
 
         return $this;
     }
@@ -76,27 +78,89 @@ class Hotel{
         return $this->chambres;
     }
 
-   
+    
     public function setChambres($chambres)
     {
         $this->chambres = $chambres;
-
+        
         return $this;
     }
-    public function addChambre(Chambres $chambre){
+    public function getReservation()
+    {
+        return $this->reservations;
+    }
+    
+    
+    public function setReservation($reservation)
+    {
+        $this->reservations = $reservation;
+    
+        return $this;
+    }
+
+    //Méthode pour ajouter des chambres au tableau de chambres
+    public function addChambre(Chambre $chambre){
         $this->chambres[] = $chambre;
     }
 
-//     public function __toString()
-//     {
-//         return "Hôtel " . $this->hotel . "****" . $this->adresse; 
-//     }
+    //Méthode pour ajouter des réservations au tableau de réservations
+    public function addReservation(Reservation $reservation){
+        $this->reservations[] = $reservation;
+    }
+ 
+    //Méthode pour compter le nombre de chambres réservées
+    public function afficherNbChambresReservees()
+    {
+        $chambreReserve =  count($this->reservations);
+        return  $chambreReserve;
+    }
+
+     //Méthode pour compter le nombre de chambre dans l'hôtel
+     public function afficherNbChambres()
+     {
+        $totalChambre = count($this->chambres);
+ 
+         return $totalChambre;
+     }
+
+     //Méthode pour compter le nombre de chambres dispos
+    public function chambreDisponible (){
+        $chambreReserve = count($this->reservations);
+        $totalChambre = count($this->chambres);
+        $chambreDispo = $totalChambre-$chambreReserve;
+        return $chambreDispo;
+    }
+
+    //Méthode pour afficher les réservations d'un hôtel
+    public function afficherReservations(){
+        echo "<br>". $result = "<br><h3>Réservations de l'Hôtel " . $this->nomHotel."</h3><br> ";
+        //si le tableau des reservation est vide 
+        if(empty($this->reservations)){
+            $result = $result . "Aucune réservation<br>";
+        }else{
+            echo count($this->reservations). " RESERVATION";
+            foreach ($this->reservations as $reservation){
+                $result = $result . $reservation->getClient()->getnom() . " " . $reservation->getClient()->getPrenom() . $reservation->getChambre()->getNomChambre() . $reservation->getDateArrivé()->format("d-m-Y") . " au ". $reservation->getDateDépart()->format("d-m-Y");
+            }
+        }
+    
+    }
+
+    
+
+    public function __toString()
+    {
+        return  $this->nomHotel . " **** " . $this->adresse . " " . $this->codePostal . " " . $this->ville;
+    }
 
     public function afficherInfos(){
-        echo $this->hotel . "****" . $this->adresse . $this->codePostal . $this->ville;
+        echo $this . "<br>" . "Nombre de chambres : " . count($this->chambres) . "<br>" . "Nombre de chambres réservées : " . count($this->reservations) . "<br>" . " Nombre de chambres dispo : " . $this->chambreDisponible() ;
     }
 
 
 
-}
 
+    
+    
+}
+  
